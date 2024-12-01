13d2b20839d65dde7eadf2e2931621049d40168b
NovaisVictor <victornovaislima@gmail.com>
2024-12-01 11:19:07 -0300

    fix: iframe size

--- src/app/(app)/aviator/page.tsx
+++ src/app/(app)/aviator/page.tsx
@@ -2,13 +2,13 @@ import SignalComponent from './signal'
 
 export default function Aviator() {
   return (
-    <div className="h-screen flex flex-col gap-12 overflow-x-hidden">
+    <div className="min-h-screen flex flex-col gap-12 overflow-x-hidden">
       <div className="">
         <SignalComponent />
       </div>
       <div className="h-full">
         <iframe
-          className="h-full w-full"
+          className="h-[700px] w-full"
           // style="height: 600px; border-radius: 25px; box-shadow: 0 0 31px -4px black;  border-width: 1px;"
           src="https://go.aff.sortenabet.com/tf9esqb9?utm_campaign=app&utm_content=luiz"
         />
