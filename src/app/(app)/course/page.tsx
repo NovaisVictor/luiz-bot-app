afd61cb1c6e3956292af56e11eac6b839d64aa85
NovaisVictor <victornovaislima@gmail.com>
2024-12-03 08:59:18 -0300

    last updates

--- src/app/(app)/course/page.tsx
+++ src/app/(app)/course/page.tsx
@@ -1,25 +1,10 @@
-'use client'
-/* eslint-disable @next/next/no-img-element */
 import { GradiantCard } from '@/components/gradiant-card'
 import { InstallAppCard } from '@/components/install-app-card'
 import { Button } from '@/components/ui/button'
 import { Play } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
 
 export default function Course() {
-  React.useEffect(() => {
-    const script = document.createElement('script')
-    script.src =
-      'https://scripts.converteai.net/cd42e2f1-44f3-470d-bb0a-328b0476d231/players/674bdeafc13d82038b390b6e/player.js'
-    script.async = true
-    document.head.appendChild(script)
-
-    // Clean up the script when the component unmounts
-    return () => {
-      document.head.removeChild(script)
-    }
-  }, [])
   return (
     <div className="flex flex-col gap-6 justify-center">
       <div className="text-center space-y-4 w-full">
@@ -44,39 +29,8 @@ export default function Course() {
           {/* 01 - <span className="text-primary">Mostrando o App</span> */}
         </h1>
         <GradiantCard>
-          <div
-            id="vid_674bdeafc13d82038b390b6e"
-            style={{
-              position: 'relative',
-              width: '100%',
-              padding: '56.25% 0 0',
-            }}
-          >
-            <img
-              id="thumb_674bdeafc13d82038b390b6e"
-              src="https://images.converteai.net/cd42e2f1-44f3-470d-bb0a-328b0476d231/players/674bdeafc13d82038b390b6e/thumbnail.jpg"
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover',
-                display: 'block',
-              }}
-              alt="thumbnail"
-            />
-            <div
-              id="backdrop_674bdeafc13d82038b390b6e"
-              style={{
-                WebkitBackdropFilter: 'blur(5px)',
-                backdropFilter: 'blur(5px)',
-                position: 'absolute',
-                top: 0,
-                height: '100%',
-                width: '100%',
-              }}
-            />
+          <div className="py-14 flex justify-center">
+            <Play className="bg-primary rounded-full p-4" size={64} />
           </div>
         </GradiantCard>
         <Button size={'sm'} className="w-11/12 mx-auto mt-4">
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,11 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+
+'use client'
 import Plus18 from '@/assets/plus-18'
 import { GradiantCard } from '@/components/gradiant-card'
 import { InstallAppCard } from '@/components/install-app-card'
 import { Button } from '@/components/ui/button'
-import { Play } from 'lucide-react'
 import Link from 'next/link'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src =
+      'https://scripts.converteai.net/cd42e2f1-44f3-470d-bb0a-328b0476d231/players/674bdeafc13d82038b390b6e/player.js'
+    script.async = true
+    document.head.appendChild(script)
+
+    // Clean up the script when the component unmounts
+    return () => {
+      document.head.removeChild(script)
+    }
+  }, [])
   return (
     <div className="flex flex-col gap-6 justify-center">
       <div className="bg-primary rounded-2xl text-center py-8 space-y-2">
@@ -15,8 +30,39 @@ export default function Home() {
         </p>
       </div>
       <GradiantCard>
-        <div className="py-14 flex justify-center">
-          <Play className="bg-primary rounded-full p-4" size={64} />
+        <div
+          id="vid_674bdeafc13d82038b390b6e"
+          style={{
+            position: 'relative',
+            width: '100%',
+            padding: '56.25% 0 0',
+          }}
+        >
+          <img
+            id="thumb_674bdeafc13d82038b390b6e"
+            src="https://images.converteai.net/cd42e2f1-44f3-470d-bb0a-328b0476d231/players/674bdeafc13d82038b390b6e/thumbnail.jpg"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              display: 'block',
+            }}
+            alt="thumbnail"
+          />
+          <div
+            id="backdrop_674bdeafc13d82038b390b6e"
+            style={{
+              WebkitBackdropFilter: 'blur(5px)',
+              backdropFilter: 'blur(5px)',
+              position: 'absolute',
+              top: 0,
+              height: '100%',
+              width: '100%',
+            }}
+          />
         </div>
       </GradiantCard>
       <GradiantCard>
