48c948707f435f70e9a19364f29d4701436a0455
NovaisVictor <victornovaislima@gmail.com>
2024-12-04 15:26:42 -0300

    last updates

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,12 +1,8 @@
-// lib/redis.js
-import Redis from 'ioredis'
+import { Redis } from '@upstash/redis'
 
-// Substitua os valores abaixo pelos valores fornecidos pelo Upstash
 const redis = new Redis({
-  host: 'assured-guinea-25510.upstash.io', // Exemplo: <your-upstash-id>.upstash.io
-  port: 6379, // Porta padrão do Redis, geralmente 6379
-  password: 'AWOmAAIjcDFjZDY1NDYyZGM5NmI0MjYzYTEzMDU5OTMxNWU3MTljNHAxMA', // A senha que você obteve no Upstash
-  tls: {}, // Ative TLS para conexões seguras
+  url: 'https://assured-guinea-25510.upstash.io',
+  token: 'AWOmAAIjcDFjZDY1NDYyZGM5NmI0MjYzYTEzMDU5OTMxNWU3MTljNHAxMA',
 })
 
 export default redis
