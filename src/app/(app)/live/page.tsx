f3aaf3fd67f86b2382b2eba6f441b0abf1d3b47b
NovaisVictor <victornovaislima@gmail.com>
2024-12-05 15:22:38 -0300

    protections signal font size

--- src/app/(app)/live/page.tsx
+++ src/app/(app)/live/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 /* eslint-disable @next/next/no-img-element */
 import Instagram from '@/assets/instagram'
 import Plus18 from '@/assets/plus-18'
@@ -6,6 +7,7 @@ import Youtube from '@/assets/youtube'
 import { GradiantCard } from '@/components/gradiant-card'
 import { InstallAppCard } from '@/components/install-app-card'
 import { Button } from '@/components/ui/button'
+import { VideoPlayer } from '@/components/video-player'
 import Link from 'next/link'
 export default function Live() {
   return (
@@ -15,52 +17,11 @@ export default function Live() {
         de 20X a 100X
       </h1>
       <GradiantCard>
-        <div
-          id="vid_674bdebceb81f8e8fb46e96c"
-          style={{
-            position: 'relative',
-            width: '100%',
-            padding: '56.25% 0 0',
-          }}
-        >
-          <img
-            id="thumb_674bdebceb81f8e8fb46e96c"
-            src="https://images.converteai.net/cd42e2f1-44f3-470d-bb0a-328b0476d231/players/674bdebceb81f8e8fb46e96c/thumbnail.jpg"
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              display: 'block',
-            }}
-            alt="thumbnail"
-          />
-          <div
-            id="backdrop_674bdebceb81f8e8fb46e96c"
-            style={{
-              WebkitBackdropFilter: 'blur(5px)',
-              backdropFilter: 'blur(5px)',
-              position: 'absolute',
-              top: 0,
-              height: '100%',
-              width: '100%',
-            }}
-          ></div>
-          <script
-            type="text/javascript"
-            id="scr_674bdebceb81f8e8fb46e96c"
-            dangerouslySetInnerHTML={{
-              __html: `
-            var s = document.createElement("script"); 
-            s.src = "https://scripts.converteai.net/cd42e2f1-44f3-470d-bb0a-328b0476d231/players/674bdebceb81f8e8fb46e96c/player.js"; 
-            s.async = true; 
-            document.head.appendChild(s);
-          `,
-            }}
-          ></script>
-        </div>
+        <VideoPlayer
+          scriptSrc="https://scripts.converteai.net/cd42e2f1-44f3-470d-bb0a-328b0476d231/players/674bdebceb81f8e8fb46e96c/player.js"
+          thumbnailSrc="https://images.converteai.net/cd42e2f1-44f3-470d-bb0a-328b0476d231/players/674bdebceb81f8e8fb46e96c/thumbnail.jpg"
+          videoId="vid_674bdebceb81f8e8fb46e96c"
+        />
       </GradiantCard>
       <GradiantCard>
         <div className="flex flex-col justify-center gap-4">
